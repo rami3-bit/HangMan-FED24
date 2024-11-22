8b738608c33ad133f0ab90d3216f79fc412302f3
AmirFrontEnd-hub <amiralziyadi@gmail.com>
2024-11-22 13:28:18 +0100

    Reverting, Button didnt show

--- javascript/script.js
+++ javascript/script.js
@@ -7,8 +7,6 @@ const answer=document.querySelector(".answer");
 
 let count = 0;
 
-const randomIndex = Math.floor(Math.random() * wordList.length);
-const { word, hint } = wordList[randomIndex];
 
 //adding Keyboard
 for(let i=97; i<=122;i++){
@@ -18,26 +16,3 @@ for(let i=97; i<=122;i++){
 	keyboard.appendChild(button);
 }
 
-const matchWord = (val) => {
-	const matches = [];
-	console.log(word);
-	word.split("").forEach((el, index) => {
-	  if (el === val.toLowerCase()) {
-		matches.push(index);
-	  }
-	});
-  
-	if (matches.length === 0) {
-	  count++;
-	  chance.innerText = `${count}/6`;
-	} else {
-	  matches.forEach((v) => {
-		const letterElem = document.querySelectorAll(".letter");
-		letterElem[v].innerText = val;
-		letterElem[v].classList.add("guess");
-	  });
-	}
-  };
-  
-	
-// Word Libariary for all the levels, each options is buttons
