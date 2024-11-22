7b1594111c77bbc273e2f31aa382389f44373e7d
Dmytro <eazy777er@gmail.com>
2024-11-23 00:16:46 +0100

    Started javascript for default leaderboard sorting

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -1,11 +1,14 @@
+// we declare variables----------------
 
 const leaderboard = document.querySelector('.whole-leaderboard')
 const LeaderboardCloseButton = document.querySelector(`.leaderboard-close-button`)
 const LeaderboardHomeButton = document.querySelector(`#leaderboard-home-button`)
 const mainMenu = document.querySelector(`.main-menu`)
+const leaderboardList = document.querySelector('.leaderboard-list')
+const sortButtons = Array.from(document.querySelectorAll('.leaderboard-sort-button'))
 
 
-// --------------------hide/show Leaderboard------------
+// --------------------hide/show Leaderboard tab------------
 
 LeaderboardHomeButton.addEventListener('click', () => {
 	leaderboard.classList.remove('hidden')
@@ -17,5 +20,24 @@ LeaderboardCloseButton.addEventListener('click', () => {
 	mainMenu.classList.remove(`hidden`)
 })
 
-// --------------------------------
+// ---------object for tracking of sorting---
 
+let sortOrder = {
+    result: true,
+    time: true,
+    date: true
+}
+
+// ----------------We create a function for converting strings (stats from leaderboard which are there by default) to arrays. Stats which we will need for comparison will be converted from string to numbers-----
+
+const parseListItem = (itemText) => {
+    const stats = itemText.split(', ')
+    return {
+        name: stats[0],
+        mistakes: parseInt(stats[1].split(': ')[1]),
+        wordLength: parseInt(stats[2].split(': ')[1]),
+        date: new Date(stats[3]),
+        time: parseInt(stats[4].split(' ')[0]),
+        status: stats[5]
+    }
+}
