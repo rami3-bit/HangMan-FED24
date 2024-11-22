dd617c7b1d88525d10c5c9d3d990f9532f1f1ec3
Rami <rami.elsaneh@protonmail.com>
2024-11-22 16:09:57 +0100

    lagt till en select function i js och css, där svart är select, grå är unselect

--- home.js
+++ home.js
@@ -1,1 +1,19 @@
-const leaderBoardVy = document.querySelector(".leaderboard-button");
+//			select diffculty
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	const buttons = document.querySelectorAll('.difficulty-selection button');
+  
+	
+	buttons.forEach(button => {
+	  button.addEventListener('click', () => {
+
+		buttons.forEach(b => b.classList.remove('selected'));
+  
+		
+		button.classList.add('selected');
+	  });
+	});
+  });
+  
+  //		Leaderboard Vy
