88457f3ac27292e44acb1be712073769cfc57156
AmirFrontEnd-hub <amiralziyadi@gmail.com>
2024-11-22 12:52:06 +0100

    Adding querys for Javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,11 @@
-const keyboard=document.querySelector('.keyboard')
+// Information Dump, all Classes/ID
+const keyboard=document.querySelector('.keyboard');
+const wordDisplay=document.querySelector('word-display');
+const mistake=document.querySelector(".mistake-count");
+const gameover=document.querySelector(".game-over");
+const answer=document.querySelector(".answer");
+
+let count = 0;
 
 //adding Keyboard
 for(let i=97; i<=122;i++){
