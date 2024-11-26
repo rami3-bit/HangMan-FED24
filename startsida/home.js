fb0b684415972c1dcaa0aa69b5bbf68500c21334
Rami <rami.elsaneh@protonmail.com>
2024-11-26 15:56:58 +0100

    jobbar med js för select diffculty

---
+++ startsida/home.js
@@ -0,0 +1,25 @@
+const easyDiv = document.querySelector(".easy");
+const mediumDiv = document.querySelector(".medium");
+const hardDiv = document.querySelector(".hard");
+		//väljer ID # i radio knapparna
+const easyRadio = document.querySelector("#easy");
+const mediumRadio = document.querySelector("#medium")
+const hardRadio = document.querySelector("#hard")
+
+easyRadio.addEventListener("change", () => updateSelection
+(easyDiv, mediumDiv, hardDiv));
+
+mediumRadio.addEventListener("change", () => updateSelection
+(easyDiv, mediumDiv, hardDiv));
+
+hardRadio.addEventListener("change", () => updateSelection
+}
+
+// Funktion för att uppdatera val
+function updateSelection(selected, ...others) {
+  // Lägg till en klass till den valda div-enheten
+  selected.classList.add('selected');
+
+  // Ta bort klassen från de andra
+  others.forEach(div => div.classList.remove('selected'));
+}
