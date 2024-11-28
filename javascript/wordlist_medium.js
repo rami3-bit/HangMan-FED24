c6bdad59c05f2923d52e94c153c449517075a6ba
Dmytro <eazy777er@gmail.com>
2024-11-28 01:24:39 +0100

    Created final_start_page html and js

---
+++ javascript/wordlist_medium.js
@@ -0,0 +1,8 @@
+export const wordList = [ 
+	"adolescences", "bestselling", "breakthrough", "childminders", "commitments", 
+	"contraption", "counterparts", "developments", "disciplines", "environments", 
+	"exaggerated", "friendliness", "grandparents", "headquarters", "illuminators", 
+	"imagination", "information", "justification", "marketplaces", "masterpieces", 
+	"notifications", "opportunities", "personalities", "photographers", "publications", 
+	"relationships", "representation", "transmissions", "unforgettable", "wildernesses"
+	  ];
